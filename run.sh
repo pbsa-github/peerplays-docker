1c7bf2264950d7e585a78392589544f655c5ac59
Robert Son Hedler <rhedler@datasecuritynode.com>
2020-04-25 06:56:15 +0000

    Revert "Changed to peerplays block files."
    This reverts commit e2a0b34c354ed8a87201f994f293ad932086bca5

--- run.sh
+++ run.sh
@@ -396,26 +396,17 @@ dlblocks() {
         if [[ "$BC_RSYNC" == "no" ]]; then
             msg red "As BC_RSYNC is set to 'no', we're just going to try to retry the http download"
             msg "If your HTTP source is uncompressed, we'll try to resume it"
-            #dl-blocks-http "$BC_HTTP" "$BC_HTTP_CMP"
-            cd $BC_FOLDER
-            rm -f .gitignore
-            git clone git@gitlab.com:robert.hedler/dlblocks.git . && rm -rf .git
+            dl-blocks-http "$BC_HTTP" "$BC_HTTP_CMP"
             return
         else
             msg green "We'll now use rsync to attempt to repair any corruption, or missing pieces from your block_log."
-            #dl-blocks-rsync "$BC_RSYNC"
-            cd $BC_FOLDER
-            rm -f .gitignore
-            git clone git@gitlab.com:robert.hedler/dlblocks.git . && rm -rf .git
+            dl-blocks-rsync "$BC_RSYNC"
             return
         fi
     fi
     msg "No existing block_log found. Will use standard http to download, and will\n also decompress lz4 while downloading, to save time."
     msg "If you encounter an error while downloading the block_log, just run dlblocks again,\n and it will use rsync to resume and repair it"
-    #dl-blocks-http "$BC_HTTP" "$BC_HTTP_CMP" 
-    cd $BC_FOLDER
-    rm -f .gitignore
-    git clone git@gitlab.com:robert.hedler/dlblocks.git . && rm -rf .git
+    dl-blocks-http "$BC_HTTP" "$BC_HTTP_CMP" 
     msg "FINISHED. Blockchain installed to ${BC_FOLDER}/block_log (make sure to check for any errors above)"
     msg red "If you encountered an error while downloading the block_log, just run dlblocks again\n and it will use rsync to resume and repair it"
     echo "Remember to resize your /dev/shm, and run with replay!"
