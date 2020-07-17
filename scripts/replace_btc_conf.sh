1bfa054a825f8a8e76a5ac1741fbfe46e55359af
Roshan Syed <roshan.syed.rs@gmail.com>
2020-07-17 18:10:17 +0000

    chore: fixed btc config replace script for linux

--- scripts/replace_btc_conf.sh
+++ scripts/replace_btc_conf.sh
@@ -2,7 +2,8 @@
 # for macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
         BTC_CONF_PATH="$(greadlink -f ../bitcoin/regtest/bitcoin.conf)"
+        sed -i '' "s|BTC_REGTEST_CONF=.*|BTC_REGTEST_CONF=\"$BTC_CONF_PATH\"|g" ../.env
 else
-	BTC_CONF_PATH="$(readlink -f ../bitcoin/regtest/bitcoin.conf)"
+        BTC_CONF_PATH="$(readlink -f ../bitcoin/regtest/bitcoin.conf)"
+        sed -i "s|BTC_REGTEST_CONF=.*|BTC_REGTEST_CONF=\"$BTC_CONF_PATH\"|g" ../.env
 fi
-sed -i '' "s|BTC_REGTEST_CONF=.*|BTC_REGTEST_CONF=\"$BTC_CONF_PATH\"|g" ../.env
