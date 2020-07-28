df1569c755acc383f70bba803c046d5da8a0bf85
roshansyed <r.syed@pbsa.info>
2020-07-28 12:10:28 -0300

    feat: added btc import scripts and updated the blockchain setup script

--- scripts/replace_btc_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# for macOS
-if [[ "$OSTYPE" == "darwin"* ]]; then
-        BTC_CONF_PATH="$(greadlink -f ../bitcoin/regtest/bitcoin.conf)"
-        sed -i '' "s|BTC_REGTEST_CONF=.*|BTC_REGTEST_CONF=\"$BTC_CONF_PATH\"|g" ../.env
-else
-        BTC_CONF_PATH="$(readlink -f ../bitcoin/regtest/bitcoin.conf)"
-        sed -i "s|BTC_REGTEST_CONF=.*|BTC_REGTEST_CONF=\"$BTC_CONF_PATH\"|g" ../.env
-fi
